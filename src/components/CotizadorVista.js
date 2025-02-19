10799e22045a0ff79009c2e05866d62326a031a8
luispal7030 <I202224764@cibertec.edu.pe>
2025-02-19 13:41:41 -0500

    SE AGREGA Y ACTUALIZA LOGOS DE LAS EMPRESAS. SE MEJORA LA INTERFAZ

--- src/components/CotizadorVista.js
+++ src/components/CotizadorVista.js
@@ -7,9 +7,14 @@ export default function CotizadorVista() {
     hoy.getMonth() + 1
   ).padStart(2, "0")}-${String(hoy.getDate()).padStart(2, "0")}`;
 
+ const optEmpresa = [
+    { empresa: "Torque-G46", src: "" },
+  
+    {  empresa: "Irontools", src: "" },];
+
   const [ruc, setRuc] = useState("");
   const [moneda, setMoneda] = useState("$.");
-  const [empresa, setEmpresa] = useState("");
+  const [empresa, setEmpresa] = useState(optEmpresa[0].empresa);
   const [fechaEmision, setFechaEmision] = useState(fechaActual);
   const [imgEmpresa, setImgEmpresa] = useState("");
 
@@ -27,159 +32,99 @@ export default function CotizadorVista() {
 
   return (
     <div className="cotizadorBox">
-      <div className="flex2 borange">
-        <section className="flexColumn gapp8">
-        <div className="red">
-          <div className="flexbox gapp4 bgreen">
-          
-            <div className="flexbox gapp4">
-            
-              <div>
-                <p>Ruc</p>
-                <input
-                  type="text"
-                  id="rucInputData"
-                  placeholder="2012345678X"
-                  value={ruc}
-                  maxLength={12}
-                  onChange={(e) => setRuc(e.target.value)}
-                  className="inputboxitm"
-                />
-              </div>
-              {ruc.length > 5 && (
-                <div>
-                  <p>Nombre</p>
-                  <input
-                    type="text"
-                    className="inputboxitm"
-                    disabled
-                    placeholder="Nombre del cliente"
-                  />
-                </div>
-              )}
-            </div>
-            <div>
-              <p>Moneda</p>
-              <select
-                name="moneda"
-                id="moneda"
-                onChange={handlechangeselectmoneda}
-                className="padd2"
-                value={moneda}
-              >
-                <option value="$.">Dolar</option>
-                <option value="S/.">Soles</option>
-              </select>
-            </div>
-            <div>
-              <p>Empresa</p>
-              <select
-                name="empresa"
-                id="empresa"
-                onChange={handlechangeselectempresa}
-                className="padd2"
-                value={empresa}
-              >
-                <option value="Torque-G46">Torque-G46</option>
-                <option value="Irontools">Irontools</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="flexbox gapp4 padd1">
-            <div className="wd2 flexColumn">
-              <p>Producto</p>
-              <input
-                type="text"
-                placeholder="Producto"
-                className="inputboxitm wd"
-              />
-            </div>
-
-            <div>
-              <p>Cantidad</p>
-              <input
-                type="number"
-                placeholder="Cantidad"
-                className="inputboxitm"
-              />
-            </div>
-
-            <div>
-              <p>Precio</p>
-              <input
-                type="number"
-                placeholder="Precio"
-                className="inputboxitm"
-              />
-            </div>
-          </div> </div> 
-          <div>
-            <img src="" alt="" className="imgicons red"/>
-          </div>
-        </section>
+      <div className="flex2 ">
 
         <section>
+          <h1 className="cBlack">Datos Cliente</h1>
           <p>Ingresa los datos del cliente a cotizar:</p>
-          <div className="flexbox">
-          <section>
-            <div>              <div>
-                <p>Ruc</p>
-                <input
-                  type="text"
-                  id="rucInputData"
-                  placeholder="2012345678X"
-                  value={ruc}
-                  maxLength={12}
-                  onChange={(e) => setRuc(e.target.value)}
-                  className="inputboxitm"
-                />
-              </div>
-              {ruc.length > 5 && (
+          <div className="flexbox wd">
+            <section className="wd85"> <div className="flexbox gapp4 padd1">
+              <div className="flexbox">
                 <div>
-                  <p>Nombre</p>
+                  <p>Ruc</p>
                   <input
                     type="text"
+                    id="rucInputData"
+                    placeholder="2012345678X"
+                    value={ruc}
+                    maxLength={12}
+                    onChange={(e) => setRuc(e.target.value)}
                     className="inputboxitm"
-                    disabled
-                    placeholder="Nombre del cliente"
                   />
                 </div>
-              )}</div>
+                {ruc.length > 5 && (
+                  <div>
+                    <p>Nombre</p>
+                    <input
+                      type="text"
+                      className="inputboxitm"
+                      disabled
+                      placeholder="Nombre del cliente"
+                    />
+                  </div>
+                )}
+              </div>
               <div>
-              <p>Moneda</p>
-              <select
-                name="moneda"
-                id="moneda"
-                onChange={handlechangeselectmoneda}
-                className="padd2"
-                value={moneda}
-              >
-                <option value="$.">Dolar</option>
-                <option value="S/.">Soles</option>
-              </select>
+                <p>Moneda</p>
+                <select
+                  name="moneda"
+                  id="moneda"
+                  onChange={handlechangeselectmoneda}
+                  className="padd2"
+                  value={moneda}
+                >
+                  <option value="$.">Dolar</option>
+                  <option value="S/.">Soles</option>
+                </select>
+              </div>
               <div>
-              <p>Empresa</p>
-              <select
-                name="empresa"
-                id="empresa"
-                onChange={handlechangeselectempresa}
-                className="padd2"
-                value={empresa}
-              >
-                <option value="Torque-G46">Torque-G46</option>
-                <option value="Irontools">Irontools</option>
-              </select>
-            </div>
-            </div>
-{/* ---------------- */}
-            <div></div>
-          </section>
-          <section>
-            <img src={""} alt=""/>
-          </section>
-          </div>
+                <p>Empresa</p>
+                <select
+                  name="empresa"
+                  id="empresa"
+                  onChange={handlechangeselectempresa}
+                  className="padd2"
+                  value={empresa}
+                >
+                  <option value="Torque-G46">Torque-G46</option>
+                  <option value="Irontools">Irontools</option>
+                </select>
+              </div>
+              </div>
+              {/* ---------------- */}
+              <div className="flexbox gapp4 padd1">
+                <div className="flexColumn">
+                  <p>Producto</p>
+                  <input
+                    type="text"
+                    placeholder="Producto"
+                    className="inputboxitm wd"
+                  />
+                </div>
 
+                <div>
+                  <p>Cantidad</p>
+                  <input
+                    type="number"
+                    placeholder="Cantidad"
+                    className="inputboxitm"
+                  />
+                </div>
+
+                <div>
+                  <p>Precio</p>
+                  <input
+                    type="number"
+                    placeholder="Precio"
+                    className="inputboxitm"
+                  />
+                </div>
+              </div>
+            </section>
+            <section className="wd3 padd1">
+              <img className="" src="" alt="" />
+            </section>
+          </div>
         </section>
 
         {/* ------------------------------ */}
@@ -233,35 +178,32 @@ export default function CotizadorVista() {
 
       <div className="flex1">
         <div className="flexbox padd1">
-        <ion-icon name="caret-forward-outline"></ion-icon>
+          <ion-icon name="caret-forward-outline"></ion-icon>
           <h1>HERRAMIENTAS</h1>
         </div>
         <section>
-        <div>
-
-          <h3 className="cBlack">Fecha Emisión</h3>
-          <input
-            type="date"
-            name="fecha_emision"
-            id="fecha_emision"
-            value={fechaEmision}
-            onChange={handleFechaChange}
-          />
-
-        </div>
-        <div>
-          <h3>Forma de pago</h3>
-          <select name="forma_pago" id="forma_pago" className="wd">
-            <option value="contado">Contado</option>
-            <option value="credito">Credito</option>
-            <option value="credito">Credito 7 Días</option>
-            <option value="credito">Credito 15 Días</option>
-            <option value="credito">Credito 30 Días</option>
-            <option value="credito">Credito 90 Dias</option>
-          </select>
-        </div>
+          <div>
+            <h3 className="cBlack">Fecha Emisión</h3>
+            <input
+              type="date"
+              name="fecha_emision"
+              id="fecha_emision"
+              value={fechaEmision}
+              onChange={handleFechaChange}
+            />
+          </div>
+          <div>
+            <h3>Forma de pago</h3>
+            <select name="forma_pago" id="forma_pago" className="wd">
+              <option value="contado">Contado</option>
+              <option value="credito">Credito</option>
+              <option value="credito">Credito 7 Días</option>
+              <option value="credito">Credito 15 Días</option>
+              <option value="credito">Credito 30 Días</option>
+              <option value="credito">Credito 90 Dias</option>
+            </select>
+          </div>
         </section>
-
 
         <div>
           <table>
@@ -284,7 +226,7 @@ export default function CotizadorVista() {
 
         <div>
           <h3 className="cBlack">Observaciones</h3>
-          <textarea className="wd" placeholder="Observaciones..."/>
+          <textarea className="wd" placeholder="Observaciones..." />
           <button className="btnSuccess" type="submit">
             GUARDAR DATOS
           </button>
